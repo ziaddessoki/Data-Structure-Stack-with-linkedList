bd3c6a0b38182ad35cfaa3bfa19f736dac1cdc64
Zee(Ziad) Eldesouky <ziad.dessoki@yahoo.com>
2020-08-07 05:11:00 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value){
+    const newNode = new Node
+    if(this.length === 0 ){
+
+    }
+  }
+  pop(){
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+
+
+
+
+//Discord
+//Udemy
+//google
